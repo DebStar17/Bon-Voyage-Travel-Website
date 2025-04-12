934361766f5871e41eab77274fb346c8e0cc1ab2
Drik0the0gen <4222713-Drik0the0gen@users.noreply.replit.com>
2025-04-12 19:10:02 +0000

    Initial commit

---
+++ code/script2.js
@@ -0,0 +1,2 @@
+gsap.fromTo('.navbar', {y:-60}, {y:2, duration:2, delay:0.6, ease:'elastic.out(1, 0.3)'});
+gsap.fromTo('.text', {width: '0%'}, {width:'5%', duration:0.8, delay:1.2, ease:'bounce.out'});
